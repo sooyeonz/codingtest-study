2fd570842d3f29bc0b823beaf64b741d11754f2d
sooyeonz <5196ksy@naver.com>
2025-11-25 09:33:54 +0900

    feat: 16. 기능 개발 solution

---
+++ src/week3/sooyeonz/n16_FeatureDevelopment.java
@@ -0,0 +1,38 @@
+package week3.sooyeonz;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class n16_FeatureDevelopment {
+
+	public static void main(String[] args) {
+
+	}
+
+	private int[] solution(int[] progresses, int[] speeds) {
+		Deque<Integer> answer = new ArrayDeque<>();
+
+		int n = progresses.length;
+		int[] daysLeft = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			daysLeft[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+		}
+
+		int count = 0;
+		int maxDay = daysLeft[0];
+
+		for (int i = 0; i < n; i++) {
+			if (daysLeft[i] <= maxDay) {
+				count++;
+			} else {
+				answer.add(count);
+				count = 1;
+				maxDay = daysLeft[i];
+			}
+		}
+
+		answer.add(count);
+		return answer.stream().mapToInt(Integer::intValue).toArray();
+	}
+}
