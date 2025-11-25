2376f15551d1aed2c2a5048d268ea6684a8d1dd8
sooyeonz <5196ksy@naver.com>
2025-11-25 09:39:09 +0900

    feat: 16. 기능 개발 테스트 케이스 추가

--- src/week3/sooyeonz/n16_FeatureDevelopment.java
+++ src/week3/sooyeonz/n16_FeatureDevelopment.java
@@ -1,22 +1,29 @@
 package week3.sooyeonz;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 
 public class n16_FeatureDevelopment {
 
 	public static void main(String[] args) {
+		int[] progresses1 = {93, 30, 55};
+		int[] speeds1 = {1, 30, 5};
+		int[] progresses2 = {95, 90, 99, 99, 80, 99};
+		int[] speeds2 = {1, 1, 1, 1, 1, 1};
 
+		System.out.println(Arrays.toString(solution(progresses1, speeds1)));
+		System.out.println(Arrays.toString(solution(progresses2, speeds2)));
 	}
 
-	private int[] solution(int[] progresses, int[] speeds) {
+	private static int[] solution(int[] progresses, int[] speeds) {
 		Deque<Integer> answer = new ArrayDeque<>();
 
 		int n = progresses.length;
 		int[] daysLeft = new int[n];
 
 		for (int i = 0; i < n; i++) {
-			daysLeft[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+			daysLeft[i] = (int)Math.ceil((100.0 - progresses[i]) / speeds[i]);
 		}
 
 		int count = 0;
