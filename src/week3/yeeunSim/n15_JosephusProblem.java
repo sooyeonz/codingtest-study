e2de4eaa512f495b89a6e80c4f6ea516ae14a82b
yeeunSim <wjdwlr03@gmail.com>
2025-11-26 23:19:50 +0900

    feat: [큐]_15_요세푸스 문제

--- src/week3/yeeunSim/n15_JosephusProblem.java
+++ src/week3/yeeunSim/n15_JosephusProblem.java
@@ -1,4 +1,30 @@
 package week3.yeeunSim;
 
+import java.util.ArrayDeque;
+
 public class n15_JosephusProblem {
+
+    public static void main(String[] args) {
+        System.out.println(solution(5, 2));
+    }
+
+    /**
+     * 시간복잡도: O(N*K)
+     */
+    private static int solution(int N, int K) {
+
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            deque.addLast(i);
+        }
+
+        while (deque.size() > 1) {
+            for (int i = 0; i < K - 1; i++) {
+                deque.addLast(deque.pollFirst());
+            }
+            deque.pollFirst();
+        }
+
+        return deque.pollFirst();
+    }
 }
