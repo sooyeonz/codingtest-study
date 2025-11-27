ad369ce7222da005e4a4f96f9dee3a9ea1d1a546
yeeunSim <wjdwlr03@gmail.com>
2025-11-27 23:40:15 +0900

    feat: [큐]_16_기능 개발

--- src/week3/yeeunSim/n16_FeatureDevelopment.java
+++ src/week3/yeeunSim/n16_FeatureDevelopment.java
@@ -1,4 +1,49 @@
 package week3.yeeunSim;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
 public class n16_FeatureDevelopment {
+
+    public static void main(String[] args) {
+        int[] progresses1 = {93, 30, 55};
+        int[] speeds1 = {1, 30, 5};
+        int[] progresses2 = {95, 90, 99, 99, 80, 99};
+        int[] speeds2 = {1, 1, 1, 1, 1, 1};
+
+        System.out.println(Arrays.toString(solution(progresses1, speeds1)));
+        System.out.println(Arrays.toString(solution(progresses2, speeds2)));
+    }
+
+    /**
+     * 시간복잡도: O(N)
+     */
+    private static int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> answer = new ArrayDeque<>();
+
+        int n = progresses.length;
+
+        int[] daysLeft = new int[n];
+        for (int i = 0; i < n; i++) {
+            daysLeft[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+        }
+
+        int count = 0;
+        int maxDay = daysLeft[0];
+
+        for (int i = 0; i < n; i++) {
+            if (daysLeft[i] <= maxDay) {
+                count++;
+            } else {
+                answer.add(count);
+                count = 1;
+                maxDay = daysLeft[i];
+            }
+        }
+
+        answer.add(count);
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
 }
+
