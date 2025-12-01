e2d31d333c97f281343247d594cd4304ba982461
yeeunSim <wjdwlr03@gmail.com>
2025-12-01 23:37:33 +0900

    feat: [해시]_18_두 개의 수로 특정값 만들기

--- src/week4/yeeunSim/n18_TwoSum.java
+++ src/week4/yeeunSim/n18_TwoSum.java
@@ -1,4 +1,28 @@
 package week4.yeeunSim;
 
+import java.util.HashSet;
+
 public class n18_TwoSum {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 2, 3, 4, 8}, 6));
+        System.out.println(solution(new int[]{2, 3, 5, 9}, 10));
+    }
+
+    /**
+     * 시간복잡도: O(N*K)
+     */
+    private static boolean solution(int[] arr, int target) {
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for (int i : arr) {
+            if (hashSet.contains(target - i)) {
+                return true;
+            }
+            hashSet.add(i);
+        }
+
+        return false;
+    }
 }
+
